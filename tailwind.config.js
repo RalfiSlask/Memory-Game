f17882055667291d1698bcf3b757cb49cb2cc005
RalfiSlask <nilssonnarki@gmail.com>
2023-12-16 18:02:01 +0100

    update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       keyframes: {
         'green-flash': {
-          '0%': { backgroundColor: '#FDA214' }, 
-          '33%': { backgroundColor: '#32CD32' },    // green color
-          '100%': { backgroundColor: '#BCCED9' },  // End with the taken color
+          '0%': { backgroundColor: '#FDA214' },
+          '33%': { backgroundColor: '#32CD32' }, // green color
+          '100%': { backgroundColor: '#BCCED9' } // End with the taken color
         },
         'red-flash': {
-          '0%': { backgroundColor: '#FDA214' }, 
-          '33%': { backgroundColor: 'red' },    // red color
-          '100%': { backgroundColor: '#304859' },  // End with original color
-        },
+          '0%': { backgroundColor: '#FDA214' },
+          '33%': { backgroundColor: 'red' }, // red color
+          '100%': { backgroundColor: '#304859' } // End with original color
+        }
       },
       animation: {
-        "green-flash": "green-flash 0.5s forwards",
-        "red-flash": "red-flash 0.5s forwards",
+        'green-flash': 'green-flash 0.5s forwards',
+        'red-flash': 'red-flash 0.5s forwards'
       }
-    },
+    }
   },
-  plugins: [],
   plugins: [
     function ({ addUtilities }) {
       const newUtilities = {
@@ -32,25 +29,25 @@ module.exports = {
           height: '0',
           borderColor: 'transparent transparent #FDA214 transparent',
           borderStyle: 'solid',
-          borderWidth: '0 20px 20px 20px',
+          borderWidth: '0 20px 20px 20px'
         },
         '.triangle-small': {
           width: '0',
           height: '0',
           borderColor: 'transparent transparent #FDA214 transparent',
           borderStyle: 'solid',
-          borderWidth: '0 10px 10px 10px',
+          borderWidth: '0 10px 10px 10px'
         },
         '.triangle-medium': {
           width: '0',
           height: '0',
           borderColor: 'transparent transparent #FDA214 transparent',
           borderStyle: 'solid',
-          borderWidth: '0 15px 15px 15px',
-        },
+          borderWidth: '0 15px 15px 15px'
+        }
       };
 
       addUtilities(newUtilities, ['responsive', 'hover']);
-    },
+    }
   ]
-}
+};
